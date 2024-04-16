502c121542eacfdcaf4783ab3308ec15d8f4b61c
Victor Bravim <victorrafaelbravim@gmail.com>
2024-04-16 12:54:35 -0300

    Mudança

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,17 +3,20 @@ export default function Card() {
     return (
         <div className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-40" style={{ marginTop: '940px' }}>
             <div className="container mx-auto flex justify-center space-x-4">
-                <div className="bg-white rounded-lg shadow-lg p-6">
-                    <h2 className="text-xl font-semibold mb-2">Card 1</h2>
-                    <p className="text-gray-600">Conteúdo do Card 1</p>
+                <div className="bg-blue-custom text-white rounded-lg shadow-lg p-6 text-center w-1/3">
+                    <h1 className="text-xl font-semibold mb-2">LEO</h1>
+                    <h2 className="text-xl font-semibold mb-2">300-1500 KM</h2>
+                    <p className="text-gray-600">(Órbita terrestre baixa)</p>
                 </div>
-                <div className="bg-white rounded-lg shadow-lg p-6">
-                    <h2 className="text-xl font-semibold mb-2">Card 2</h2>
-                    <p className="text-gray-600">Conteúdo do Card 2</p>
+                <div className="bg-blue-custom text-white rounded-lg shadow-lg p-6 text-center w-1/3">
+                <h1 className="text-xl font-semibold mb-2">Hi-SPEED</h1>
+                    <h2 className="text-xl font-semibold mb-2">22,236 MIL</h2>
+                    <p className="text-gray-600">(Órbita geoestacionária)</p>
                 </div>
-                <div className="bg-white rounded-lg shadow-lg p-6">
-                    <h2 className="text-xl font-semibold mb-2">Card 3</h2>
-                    <p className="text-gray-600">Conteúdo do Card 3</p>
+                <div className="bg-blue-custom text-white rounded-lg shadow-lg p-6 text-center w-1/3">
+                <h1 className="text-xl font-semibold mb-2">TERRA</h1>
+                    <h2 className="text-xl font-semibold mb-2">365 DIAS</h2>
+                    <p className="text-gray-600">(Período orbital)</p>
                 </div>
             </div>
         </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,14 @@
 // Footer.tsx
+import Image from "next/image";
+import logo from '@/assets/logo.png'
 
 export default function Footer() {
     return (
         <footer className="bg-black text-white p-8">
             <div className="container mx-auto flex items-center justify-between">
                 <div className="flex items-center">
-                    <img src="/logo.png" alt="Logo" className="h-8 mr-2" />
-                    <span className="font-bold">Nome da Empresa</span>
+                <Image src={logo} alt="Logo" width={32} height={32} />
+                    <span className="font-bold">SkySat</span>
                 </div>
                 <div className="flex items-center space-x-4">
                     <a href="#" className="hover:text-blue-500">Link 1</a>
